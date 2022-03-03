e70dfb5472851f345880d60847bfda3a01ea7e53
Tom Cowland <tom@foundry.com>
2022-03-03 01:18:38 +0000

    [Plugin] Add basic resolution of `ExternalReference` `target_url` through OpenAssetIO
    Adds simple resolution of entity references set in the target_url
    property of an OTIO ExternalReference through OpenAssetIO.
    
    This currently requires that the media linker is configured via the
    media_linker_argument_map provided to otio.adapters.read_from_string.
    
    There are a variety of options for how we could improve this over time
    (through clip + inherited metadata, env var to settings file, etc.), we
    just wanted to get things going at this point to better understand
    the rest of the implementation.
    
    The additional steps required to grab the OpenAssetIO dependencies are
    a little bit cellotape and string at this point. We need to properly
    sort out how we want to manage the release of example code that is part
    of the main OpenAssetIO project.
    
    Part of #215.

--- README.md
+++ README.md
@@ -10,18 +10,81 @@ the plugin's media linker is enabled.
 > Note: This project is currently in it's early alpha stages, more
 > information and features coming soon!
 
-## Testing
+## Configuration
 
-Initial setup
+The media linker currently takes the following arguments, `settings` is
+optional, and should be any required settings for the chosen manager:
+
+```
+{
+    "identifier": "<required manager identifier>"
+    "settings": { ...  }
+}
+```
+
+Eg:
+
+```python
+linker_args = {
+    "identifier": "org.openassetio.examples.manager.bal",
+    "settings": {
+        "library_path": "my_library.json"
+    }
+}
+
+timeline = otio.adapters.read_from_string(
+    otio_str,
+    media_linker_name="openassetio_media_linker",
+    media_linker_argument_map=linker_args
+)
+```
+
+## Setup and testing
+
+At this stage, due to the alpha nature of this project, and OpenAssetIO
+itself, setup is somewhat manual.
+
+First, clone this repository:
+
+```shell
+git clone https://github.com/TheFoundryVisionmongers/otio-openassetio
+cd otio-openassetio
+```
+
+We recommend a local venv, but feel feel to adapt this to your needs:
 
 ```shell
 python -m venv .venv
 . .venv/bin/activate
+```
+
+### Dependencies
+
+To use the linker in OpenTimelineIO `openassetio` needs to be installed.
+At present, this requires installation from source:
+
+```shell
+mkdir dependencies
+git clone git@github.com:TheFoundryVisionmongers/OpenAssetIO.git dependencies/OpenAssetIO
+pip install dependencies/OpenAssetIO
+```
+
+### OpenTimelineIO plugin
+
+The dev extras will also install the required testing dependencies.
+
+```shell
 pip install -e '.[dev]'
 ```
 
-```python
-pytest
+### Testing
+
+To run the tests we need the `BasicAssetLibrary` example manager plugin from
+the OpenAssetIO repository.
+
+```shell
+export OPENASSETIO_PLUGIN_PATH=dependencies/OpenAssetIO/resources/examples/manager/BasicAssetLibrary/plugin
+pytest tests
 ```
 
 ## Licensing
--- otio_openassetio/operations/openassetio_media_linker.py
+++ otio_openassetio/operations/openassetio_media_linker.py
@@ -3,21 +3,173 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 """
-A media linker that resolves media reference URLs via OpenAssetIO if they
-contain a valid entity reference.
+A media linker that resolves media reference URLs via OpenAssetIO if
+their target_url is set to a valid entity reference.
 """
 
+from collections import namedtuple
 
-import re
+from openassetio import log
+from openassetio.hostAPI import HostInterface, Session
+from openassetio.specifications import LocaleSpecification
+from openassetio.pluginSystem import PluginSystemManagerFactory
+
 import opentimelineio as otio
+
+
+#
+# Media Linker
+#
 
 
 def link_media_reference(in_clip, media_linker_argument_map):
 
+    # Once https://github.com/TheFoundryVisionmongers/OpenAssetIO/issues/247
+    # is implemented, we can more concisely handle ImageSequenceReference,
+    # but we will need to convert file:///a/path/sequence.%04d.ext to the
+    # component fields required by OTIO. For now, we only handle simple
+    # ExternalReference schemas.
+    # We can then also set all other relevant properties of the
+    # Clip/MediaReference based on trait properties.
+    # For now, keep it simple and just resolve the URL
+
     mr = in_clip.media_reference
 
-    if not mr:
+    if not isinstance(mr, otio.schema.ExternalReference):
         return
 
-    if isinstance(mr, otio.schema.ExternalReference):
-        mr.target_url = "<resolved>"
+    session_state = _sessionState(media_linker_argument_map)
+    manager = session_state.session.currentManager()
+
+    if not extract_one(manager.isEntityReference([mr.target_url])):
+        return
+
+    context = session_state.context
+    context.locale.clipName = in_clip.name
+
+    mr.target_url = extract_one(
+        manager.resolveEntityReference([mr.target_url], context)
+    )
+
+
+#
+# OpenAssetIO classes
+#
+
+# Due to the way OTIO imports plugins, we can't factor these out into
+# their own module. It thinks this code is part of the the
+# opentimelineio.adapters module:
+#
+#   from .shared import OTIOHostInterface
+#   > ModuleNotFoundError: No module named 'opentimelineio.adapters.shared'
+#
+# Its not really a problem right now as this is the only place we need
+# them.
+
+
+class OTIOHostInterface(HostInterface):
+    """
+    An OpenAssetIO HostInterface implementation for the OTIO media linker
+    """
+
+    def identifier(self):
+        return "com.foundry.otio-openassetio.medialinker"
+
+    def displayName(self):
+        return "OpenTimelineIO OpenAssetIO Media Linker plugin"
+
+
+class OTIOClipLocale(LocaleSpecification):
+    """
+    An OpenAssetIO Locale that represents API calls for a track clip
+    """
+
+    _type = "timeline.track.clip.otio"
+
+    clipName = LocaleSpecification.TypedProperty(
+        str,
+        doc="The name of the clip in the timeline for which the reference is being resolved.",
+    )
+
+
+#
+# Helpers
+#
+
+# Until we have singular conveniences in hostAPI.Manager (see
+# https://github.com/TheFoundryVisionmongers/OpenAssetIO/issues/107),
+# then we need to unpack the batch result and raise any exceptions
+# returned for our singular reference.
+
+
+def extract_one(results):
+    """
+    A convenience to extract the first result from a batch OpenAssetIO
+    result, raising if it is an exception.
+    """
+    result = results[0]
+    if isinstance(result, Exception):
+        raise result
+    return result
+
+
+#
+# Session Management
+#
+
+# Ideally we would tie a session into the lifetime of a media linker, but
+# as they're free functions, rather than instanitated classes, we don't
+# know when they are needed. This simple/naive approach effectively
+# persists a session/context for as long as the args to the linker are
+# the same. This is not ideal. The context lifetime should really be
+# tied to each specific Timeline. That is an exercise for another day.
+
+SessionState = namedtuple("SessionState", ("session", "context"))
+
+_last_args = None
+_session_state = None
+
+
+def _sessionState(args: dict) -> Session:
+    """
+    Returns a SessionState configured for the supplied settings. If the
+    settings are the same as the last invocation, the previously
+    constructed state will be reused.
+    """
+
+    global _last_args
+    global _session_state
+
+    if args != _last_args:
+        _session_state = None
+
+    if _session_state is None:
+        _last_args = args
+        _session_state = _createSessionState(args)
+
+    return _session_state
+
+
+def _createSessionState(args: dict) -> SessionState:
+    """
+    Configures a new SessionState with the manager + settings from the
+    supplied args. A new Context is created and configured for read NB
+    with the correct locale.
+    """
+
+    host = OTIOHostInterface()
+    logger = log.SeverityFilter(log.ConsoleLogger())
+    factory = PluginSystemManagerFactory(logger)
+
+    session = Session(host, logger, factory)
+    session.useManager(args["identifier"], args.get("settings", {}))
+
+    # The lifetime of the context would ideally be tied to each specific
+    # call to read_from_string or similar. Maybe we could introspect
+    # each clip and see which Timeline it belongs to. This will do for
+    # now though and is better than making a context per clip.
+    context = session.createContext()
+    context.access = context.kRead
+    context.locale = OTIOClipLocale()
+
+    return SessionState(session=session, context=context)
